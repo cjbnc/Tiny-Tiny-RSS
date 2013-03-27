2fe4ad7426c825c2a18414aebc4c10e8b2359fd7
Charles Brabec <cjbrabec@gmail.com>
2013-03-27 12:40:53 -0400

    added DISABLE_HEADLINE_POPUPS pref

--- schema/ttrss_schema_mysql.sql
+++ schema/ttrss_schema_mysql.sql
@@ -352,6 +352,8 @@ insert into ttrss_prefs (pref_name,type_id,def_value,short_desc,section_id) valu
 
 insert into ttrss_prefs (pref_name,type_id,def_value,short_desc,section_id) values('SHOW_CONTENT_PREVIEW', 1, 'true', 'Show content preview in headlines list',2);
 
+insert into ttrss_prefs (pref_name,type_id,def_value,short_desc,section_id) values('DISABLE_HEADLINE_POPUPS', 1, 'false', 'Disable popup previews on mouseover in the headline list' ,2);
+
 insert into ttrss_prefs (pref_name,type_id,def_value,short_desc,section_id) values('SHORT_DATE_FORMAT', 2, 'M d, G:i', 'Short date format',3);
 insert into ttrss_prefs (pref_name,type_id,def_value,short_desc,section_id) values('LONG_DATE_FORMAT', 2, 'D, M d Y - G:i', 'Long date format',3);
 
